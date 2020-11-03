ef7a0219720f97b3aa52eecd86fe59aa2dfadc63
aditya02pandey <73427763+aditya02pandey@users.noreply.github.com>
2020-11-03 18:05:32 +0530

    UpdateTest

--- src/com/hsbc/exam/TextNote.java
+++ src/com/hsbc/exam/TextNote.java
@@ -18,34 +18,3 @@ public class TextNote {
 
 }
 
-
-// TextAndImageNote class
-
-package com.test;
-
-public class TextAndImageNote {
-
-	private String note ;
-	private String url ;
-	
-	public TextAndImageNote(String note, String url) {
-		// TODO Auto-generated constructor stub
-		this.note = note ;
-		this.url = url;
-	}
-	
-	public String getNote() {
-		return note;
-	}
-	public void setNote(String note) {
-		this.note = note;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	
-	
-}
